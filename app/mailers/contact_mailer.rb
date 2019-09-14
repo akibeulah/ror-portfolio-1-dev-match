80810bfc2d4f6cea61d787725dfccfbb5bdf65fd
Akindele Beulah <akibeulah@gmail.com>
2019-09-14 23:24:26 +0100

    Naming correction, heroku mailer fix and personal to work email switch

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'akibeulah@gmail.com'
+  default to: 'ultratechnologies.dev@gmail.com'
 
   def contact_email(name, email, comment)
     @name = name
