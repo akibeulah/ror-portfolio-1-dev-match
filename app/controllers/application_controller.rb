3551f79a99a812b157d61af1098cb19f9e5fb630
Akindele Beulah <akibeulah@gmail.com>
2019-09-20 10:27:21 +0100

    Redirected first time login to create profile form.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,18 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:signup, keys: [:stripe_card_token, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:signup, keys: [:stripe_card_token, :email, :password, :password_confirmation, :login_count])
   end
+
+  private
+
+  def after_sign_in_path_for(resource)
+    if current_user.login_count == 0
+      new_user_profile_path(user_id: current_user.id)
+    else
+      root_path
+    end
+    #current_user.login_count
+  end
+
 end
