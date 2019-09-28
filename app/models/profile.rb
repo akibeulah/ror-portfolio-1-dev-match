71a161c341eb385d035ab5c2f2c29e63fe34834e
Akindele Beulah <akibeulah@gmail.com>
2019-09-28 11:51:57 +0100

    Profile picture size fix.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,7 @@ class Profile < ActiveRecord::Base
   belongs_to :user
     
     has_attached_file :avatar, :styles => {
-            :medium => "400*400",
+            :medium => "2500*250",
             :thumb => "100*100"
     }, :default_url => "user_default.jpg"
 
