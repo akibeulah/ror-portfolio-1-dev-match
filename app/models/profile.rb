10ecb5b90e573a76ce9564600b500e42ba6c0eb2
Akindele Beulah <akibeulah@gmail.com>
2019-09-28 11:55:53 +0100

    Heroku profile fix

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,7 @@ class Profile < ActiveRecord::Base
   belongs_to :user
     
     has_attached_file :avatar, :styles => {
-            :medium => "2500*250",
+            :medium => "250*250",
             :thumb => "100*100"
     }, :default_url => "user_default.jpg"
 
