dce2adc7b68ff23b264edae7cbdf57cee76da5f4
Akindele Beulah <akibeulah@gmail.com>
2019-09-28 11:14:39 +0100

    Further styling and finisheing card view for show#index. "

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,7 @@ class Profile < ActiveRecord::Base
     has_attached_file :avatar, :styles => {
             :medium => "300*300",
             :thumb => "100*100"
-    }, :default_url => "/images/:style/missing.png"
+    }, :default_url => "user_default.jpg"
 
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
