625174908898639fdf088ef8ed8705341f054f91
Akindele Beulah <akibeulah@gmail.com>
2020-05-26 04:05:00 +0100

    Revert "Added new plan, and some better styling."
    This reverts commit 7b15235d6b95a2666bffd89ebdb08b8a8bd7933a.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,10 @@
 class PagesController < ApplicationController
   def home
-    @free_plan = Plan.find(1)
-    @basic_plan = Plan.find(2)
-    @pro_plan = Plan.find(3)
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
 
   def about
-  end
 
-  def plan
-    @free_plan = Plan.find(1)
-    @basic_plan = Plan.find(2)
-    @pro_plan = Plan.find(3)
   end
 end
